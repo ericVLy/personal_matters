3da199ebece3d8440e49a41ef66aa771fee1a1b7
linye <ericisnotrealname@protonmail.com>
2024-03-23 16:35:02 +0800

    logging

--- personal_matters/settings/dev.py
+++ personal_matters/settings/dev.py
@@ -28,3 +28,41 @@ except ImportError:
         }
     }
 
+    __base_path__ = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+    __log_path__ = os.path.join(__base_path__, "log")
+
+    if not os.path.exists(__log_path__):
+        os.makedirs(__log_path__)
+    from datetime import datetime
+
+    LOGGING = {
+        "version": 1,
+        "disable_existing_loggers": False,
+        "formatters": {
+            "verbose": {
+                "format": "[{levelname}][{asctime}][{module}][{process:d}][{thread:d}][{message}]",
+                "style": "{",
+            },
+            "simple": {
+                "format": "{levelname} {message}",
+                "style": "{",
+            },
+        },
+        "handlers": {
+            "file": {
+                'formatter': 'verbose',
+                "level": "INFO",
+                "class": "logging.FileHandler",
+                "filename": os.path.join(__log_path__, f"django_logfile_{datetime.now()}.log"),
+            },
+        },
+        "loggers": {
+            "django": {
+                "handlers": ["file"],
+                "level": "INFO",
+                "propagate": True,
+            },
+        },
+    }
+
+
